233884fa3a8e8dce1284367ecc66b468c2c8a70f
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-18 12:03:12 +0800

    edu

--- app/models/concerns/rails_edu_student.rb
+++ app/models/concerns/rails_edu_student.rb
@@ -3,7 +3,7 @@ module RailsEduStudent
   included do
     has_many :course_students, as: :student, dependent: :destroy
     has_many :courses, through: :course_students
-    has_many :exams, dependent: :destroy
+    #has_many :exams, dependent: :destroy
 
     has_many :crowd_students, as: :student, dependent: :destroy
     has_many :crowds, through: :crowd_students
--- app/models/rails_edu/lesson_student.rb
+++ app/models/rails_edu/lesson_student.rb
@@ -4,12 +4,21 @@ class LessonStudent < ApplicationRecord
   belongs_to :student, polymorphic: true
   belongs_to :course_student
 
+
+  has_one :card_log, ->(o){ where(card_id: o.student.card_id) }, as: :source
+
+
   before_validation :sync_course_student
+  after_commit :sync_card_log
 
   def sync_course_student
     self.course_id = course_student.course_id
     self.student_type = course_student.student_type
     self.student_id = course_student.student_id
+  end
+
+  def sync_card_log
+    
   end
 
 end
