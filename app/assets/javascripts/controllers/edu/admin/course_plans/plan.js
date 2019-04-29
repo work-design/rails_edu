fc9814020d2ff161500b2428dd4e002513635981
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-29 10:41:58 +0800

    support new

--- app/assets/javascripts/controllers/edu/admin/course_plans/plan.js
+++ app/assets/javascripts/controllers/edu/admin/course_plans/plan.js
@@ -1,30 +1,7 @@
-var url = new URL(window.location.href);
-var id = url.href.match(/course_crowds\/\d+\/course_plans/).toString().replace('course_crowds/', '').replace('/course_plans', '');
+//= require rails_booking/time_plan
+
+var id = window.location.href.match(/course_crowds\/\d+\/course_plans/).toString().replace('course_crowds/', '').replace('/course_plans', '');
 var request_url = '/CourseCrowd/' + id +'/time_plans';
-
-$('#time_plan_room_id').dropdown();
-
-$('#time_plan_time_list_id').dropdown({
-  onChange: function(value, text, $selectedItem){
-    var repeat_url = new URL(window.location);
-    repeat_url.pathname = repeat_url + '/calendar';
-    repeat_url.searchParams.set('time_list_id', value);
-    repeat_url.searchParams.set('repeat_type', document.getElementById('time_plan_repeat_type').value);
-
-    Rails.ajax({url: repeat_url, type: 'GET', dataType: 'script'});
-  }
-});
-$('#time_plan_repeat_type').dropdown({
-  onChange: function(value, text, $selectedItem){
-    var repeat_url = new URL(window.location);
-    repeat_url.pathname = request_url + '/calendar';
-    repeat_url.searchParams.set('repeat_type', value);
-    repeat_url.searchParams.set('time_list_id', document.getElementById('time_plan_time_list_id').value);
-
-    Rails.ajax({url: repeat_url, type: 'GET', dataType: 'script'});
-  }
-});
-
 
 remote_js_load(request_url);
 
