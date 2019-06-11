0c72971aa2e46ba34f00edf174e50fecd5765431
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-11 15:27:21 +0800

    course student page

--- app/models/rails_edu/plan_item.rb
+++ app/models/rails_edu/plan_item.rb
@@ -1,17 +1,18 @@
 module RailsEdu::PlanItem
   extend ActiveSupport::Concern
+  
   included do
     belongs_to :course, optional: true
-    belongs_to :lesson, optional: true
+    belongs_to :crowd, optional: true
     belongs_to :teacher, optional: true
     belongs_to :room, optional: true
-
-    has_many :lesson_students, dependent: :nullify
+    
+    belongs_to :lesson, optional: true
   end
   
   
   def students
-    lesson_students.pluck(:student_type, :student_id).map { |i| i.join('_') }
+    plan_attenders.where(attended: true).pluck(:attender_type, :attender_id).map { |i| i.join('_') }
   end
   
 end
