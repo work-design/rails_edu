61528a59957d1e5154355bf4bacf573c15ac6f5d
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-11 11:39:26 +0800

    add plan items relation

--- app/models/rails_edu/teacher.rb
+++ app/models/rails_edu/teacher.rb
@@ -1,8 +1,8 @@
 module RailsEdu::Teacher
   extend ActiveSupport::Concern
-  included do
-    has_many :course_plans
   
+  included do
+    has_many :plan_items, foreign_key: :teacher_id, dependent: :nullify
     has_many :review_exams, class_name: 'Exam', foreign_key: :reviewer_id, inverse_of: 'reviewer'
   end
 
--- app/models/teacher.rb
+++
@@ -1,3 +0,0 @@
-class Teacher < Member
-  include RailsEdu::Teacher
-end unless defined? Teacher
