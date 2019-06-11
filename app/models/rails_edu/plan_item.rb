c0d6a8907fe4a1744e26a510ff38ac6e23a518e4
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-11 15:44:10 +0800

    fix relation teacher

--- app/models/rails_edu/plan_item.rb
+++ app/models/rails_edu/plan_item.rb
@@ -2,10 +2,10 @@ module RailsEdu::PlanItem
   extend ActiveSupport::Concern
   
   included do
+    belongs_to :teacher, class_name: 'Member', optional: true
+    belongs_to :room, optional: true
     belongs_to :course, optional: true
     belongs_to :crowd, optional: true
-    belongs_to :teacher, optional: true
-    belongs_to :room, optional: true
     
     belongs_to :lesson, optional: true
   end
