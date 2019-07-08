acbc2bddb822b645748a7eec5622e51a57e0581e
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-08 15:11:10 +0800

    edit department grant

--- app/assets/javascripts/controllers/edu/admin/course_plans/new.js
+++
@@ -1,3 +0,0 @@
-$('#course_plan_lesson_id').dropdown();
-$('#course_plan_room_id').dropdown();
-$('#course_plan_teacher_id').dropdown();
--- app/assets/javascripts/controllers/edu/admin/course_plans/plan.js
+++
@@ -1,8 +0,0 @@
-//= require rails_booking/time_plan
-
-var id = window.location.href.match(/course_crowds\/\d+\/course_plans/).toString().replace('course_crowds/', '').replace('/course_plans', '');
-var request_url = '/CourseCrowd/' + id +'/time_plans';
-
-remote_js_load(request_url);
-
-$('.ui.floating.dropdown.labeled').dropdown();
