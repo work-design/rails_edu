6b00be075f2f21bd5d329925015c4a3cefda99d4
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-09 09:48:38 +0800

    moveout user

--- app/models/rails_edu/user.rb
+++
@@ -1,9 +0,0 @@
-module RailsEdu::User
-  extend ActiveSupport::Concern
-  
-  included do
-    has_many :proteges, through: :profiles
-    has_many :maintains, through: :proteges, source: :maintains
-  end
-  
-end
