101ca35a50ee44f1cff6de46424659322681b21e
qinmingyuan <mingyuan0715@foxmail.com>
2019-09-03 21:41:45 +0800

    move tutelage

---
+++ app/models/rails_edu/tutelage.rb
@@ -0,0 +1,16 @@
+module RailsProfile::Tutelage
+  extend ActiveSupport::Concern
+  
+  included do
+    has_many :crowd_students
+    has_many :crowds, through: :crowd_students
+  end
+
+  def join_crowd(crowd_id)
+    cs = self.crowd_students.build
+    cs.student = pupil
+    cs.crowd_id = crowd_id
+    cs.save
+  end
+
+end
